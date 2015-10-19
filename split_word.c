3afd6497f78ebbde65bdf95649e2a6146f77c521
AHEADer <dqawveird@gmail.com>
2015-10-20 06:13:37 +0800

    seedpk split tree

---
+++ split_word.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*ID选择器:#定义
+ *Class选择器:.定义
+ *
+ */
+int line = 0, column = 0;
+char c;
+
+enum Category {
+    Sentence,
+    ID_selector,
+    Class_selector,
+    Symbol,
+};
+
+void nextchar()
+{
+    c = getchar();
+    column += 1;
+}
+
+int spilt(const char* _argv);
+
+int main(int argc, char const *argv[])
+{
+    if(argc<2){
+        printf("input error!\n");
+        exit(-1);
+    }
+    spilt(argv[1]);
+    
+    return 0;
+
+}
+
+int spilt(const char* _argv)
+{
+    if(!freopen(_argv,"r",stdin)){
+        printf("cannot read file!");
+        return -1;
+    }
+	while(c!=EOF){
+		nextchar();
+		putchar(c);
+	}
+
+}
+
